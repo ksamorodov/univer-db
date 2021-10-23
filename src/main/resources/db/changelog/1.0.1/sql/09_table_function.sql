1116f2ea68849324ed0d002184755e7abf7dbbf8
ksamorodov <ksamorodov@yandex.ru>
2021-10-23 15:20:26 +0300

    refactor functions

--- src/main/resources/db/changelog/1.0.1/sql/09_table_function.sql
+++ src/main/resources/db/changelog/1.0.1/sql/09_table_function.sql
@@ -3,7 +3,8 @@ create sequence if not exists employee_function_seq start 1;
 CREATE TABLE if not exists employee_function
 (
     employee_function_id bigint unique not null default nextval('employee_function_seq' :: regclass),
-    condition_id bigint not null references conditions(condition_id)
+    condition_id bigint not null references conditions(condition_id),
+    name varchar(300) not null
     );
 
 alter table employee_function
